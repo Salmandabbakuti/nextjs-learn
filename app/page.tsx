5f5f40726ca6c6c3dd46cd0fe712527b5d4c5f9a
Salman Dabbakuti <dabbakutisalman@gmail.com>
2023-10-27 16:39:38 +0530

    chapter 1,2,3

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image';
 import AcmeLogo from '@/app/ui/acme-logo';
+import styles from '@/app/ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts';
 
 import Link from 'next/link';
 export default function Page() {
@@ -9,7 +12,13 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div className={styles.shape}></div>
+          <div
+            className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px border-b-black border-l-transparent border-r-transparent"
+          />
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -25,6 +34,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
